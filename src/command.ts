79db8a1bf882e763ffe76ba39e7891780c78e863
MamoruDS <mamoruds.io@gmail.com>
2020-02-21 13:49:02 +0900

    fixed regexp now match command with word char & \.\-\_ only
    improved cmdMatch input parameter

--- src/command.ts
+++ src/command.ts
@@ -1,16 +1,16 @@
-export const cmdMatch = (cmd: string, str: string): string[] | null => {
-    const regex = new RegExp(`(^\\/(${cmd}))|([^\\s]{1,})`, 'g')
-    let result = regex.exec(str)
+export const cmdMatch = (input: string): string[] | null => {
+    const regex = new RegExp(`(^\\/([\\w|_|\\-|\\.]{1,})\\s)|([^\\s]{1,})`, 'g')
+    let result = regex.exec(input)
     /**
      * result [0] -> input
-     * result [1] -> group_1 \cmd
+     * result [1] -> group_1 \/cmd\s
      * result [2] -> group_2 cmd
      * result [3] -> group_3 args
      */
     let args = []
-    args.push(result ? result[2] : null)
-    if (args[0] === cmd) {
-        while ((result = regex.exec(str)) !== null) args.push(result)
+    args.push(result ? result[2] : undefined)
+    if (args[0] !== undefined) {
+        while ((result = regex.exec(input)) !== null) args.push(result[3])
         return args
     } else {
         return null
