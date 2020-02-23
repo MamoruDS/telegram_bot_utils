d7866b19c5153cac47b9f7e6b5faf11943dde9d2
MamoruDS <mamoruds.io@gmail.com>
2020-02-23 20:51:55 +0900

    updated cache.ts with new methods

--- src/cache.ts
+++ src/cache.ts
@@ -1,19 +1,91 @@
 import * as fs from 'fs'
+import * as path from 'path'
+import * as _ from 'lodash'
 import * as low from 'lowdb'
 import * as FileSync from 'lowdb/adapters/FileSync'
 
-const CACHE_DIR = './temp'
-const CACHE_PATH = './temp/cache.json'
+import * as utils from './utils'
 
-if (!fs.existsSync(CACHE_DIR)) {
-    fs.mkdirSync(CACHE_DIR)
+const DATA_DIR = './data'
+
+// const CACHE_PATH = path.join(DATA_DIR, 'cache.json')
+const DATA_PATH = path.join(DATA_DIR, 'data.json')
+
+if (!fs.existsSync(DATA_DIR)) {
+    fs.mkdirSync(DATA_DIR)
 }
 
-if (!fs.statSync(CACHE_DIR).isDirectory()) {
+if (!fs.statSync(DATA_DIR).isDirectory()) {
     process.exit(1)
 }
 
-const adapter = new FileSync(CACHE_PATH)
+const adapter = new FileSync(DATA_PATH)
 const db = low(adapter)
 
-db.defaults({}).write()
+db.defaults({ callback_data: {} }).write()
+
+export const setCallbackData = (data: string): string => {
+    const id = utils.genId('C')
+    db.set(['cache', 'callback_data', id], data).write()
+    return id
+}
+
+export const getCallbackData = (id: string): string => {
+    return db.get(['cache', 'callback_data', id]).value()
+}
+
+export const delCallbackData = (id: string): void => {
+    db.unset(['cache', 'callback_data', id]).write()
+}
+
+export const setApplicationData = (
+    botId: string,
+    application: string,
+    chatId: string,
+    userId: string,
+    data: object
+): string => {
+    const id = utils.genId('D')
+    if (db.get(['bots', botId, application]).value() === undefined) {
+        db.set(['bots', botId, application], []).write()
+    }
+    db.set(['bots', botId, application], {
+        id: id,
+        chat_id: chatId,
+        user_id: userId,
+        data: data,
+    }).write()
+    return id
+}
+
+export const getApplicationData = (
+    botId: string,
+    application: string,
+    chatId: string,
+    userId: string
+): object => {
+    return db
+        .get(['bots', botId, application])
+        .filter({ chat_id: chatId, user_id: userId })
+        .value()
+}
+
+export const setApplicationDataByPath = (
+    botId: string,
+    application: string,
+    chatId: string,
+    userId: string,
+    dataPath: [string] | [],
+    value: any
+) => {
+    let _data = getApplicationData(botId, application, chatId, userId)
+    if (_data !== undefined) {
+        setApplicationData(
+            botId,
+            application,
+            chatId,
+            userId,
+            _.set(_data, dataPath, value)
+        )
+    }
+}
