5ef87c0d8af8240b0ffa3db7ca5bc3eb9f8d0917
MamoruDS <mamoruds.io@gmail.com>
2020-02-26 02:39:46 +0900

    added group.ts

---
+++ src/group.ts
@@ -0,0 +1,15 @@
+import { Message } from './tgTypes'
+
+export const joinListener = (msg: Message, func: (msg: Message) => any) => {
+    const newMembers = msg.new_chat_members ? msg.new_chat_members : []
+    if (newMembers.length !== 0) {
+        func(msg)
+    }
+}
+
+export const leftListener = (msg: Message, func: (msg: Message) => any) => {
+    const leftMember = msg.left_chat_member ? msg.left_chat_member : undefined
+    if (leftMember) {
+        func(msg)
+    }
+}
