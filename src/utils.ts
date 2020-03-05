1719e8934663b950f07bdcb5502ab7b265f852ce
MamoruDS <mamoruds.io@gmail.com>
2020-03-06 05:39:02 +0900

    added method genRandom
    improved method genId with new parameter 'suffix' and using genRandom now
    
    added class GroupId for generation group of ids

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,25 @@
-export const genId = (prefix?: string) => {
-    return `${prefix ? `${prefix}-` : ''}${Math.random()
-        .toString(36)
-        .substring(2, 8)}-${Date.now().toString(16)}-${Math.random()
-        .toString(36)
-        .substring(2, 8)}`.toUpperCase()
+import * as _ from 'lodash'
+
+export const genRandom = (
+    length: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+): string => {
+    return _.random(36 ** (length - 1), 36 ** length - 1).toString(36)
+}
+
+export const genId = (prefix?: string, suffix?: string) => {
+    return `${prefix ? `${prefix}-` : ''}${genRandom(6)}-${Date.now().toString(
+        16
+    )}-${genRandom(6)}${suffix ? `-${suffix}` : ''}`.toUpperCase()
+}
+
+export class GroupId {
+    private readonly _group: string
+    private readonly _prefix?: string
+    constructor(prefix?: string) {
+        this._group = genRandom(10)
+        this._prefix = prefix
+    }
+    public genId(): string {
+        return genId(this._prefix, this._group)
+    }
 }
