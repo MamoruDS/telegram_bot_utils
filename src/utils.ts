9929a0560d113e581f9cd095dfd2f3246f28699c
MamoruDS <mamoruds.io@gmail.com>
2020-03-06 06:03:09 +0900

    added idFormat for length(format definition of id string
    added regexp idRegex for id string

--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,42 @@
 import * as _ from 'lodash'
 
-export const genRandom = (
-    length: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
-): string => {
+const idFormat = {
+    prefix_length_min: 0,
+    prefix_length_max: 4,
+    suffix_length_min: 10,
+    suffix_length_max: 10,
+    phrase_1_length: 6,
+    phrase_2_length: 6,
+    group_length: 10,
+}
+
+const idRegex = new RegExp(
+    /((^\w{1,})-|)(\w{1,})-([A-F\d]{1,})-(\w{1,})(-(\w{1,})|)$/,
+    'gm'
+)
+
+export const genRandom = (length: number): string => {
+    if (length < 1 || length > 10) {
+        throw new RangeError()
+    }
     return _.random(36 ** (length - 1), 36 ** length - 1).toString(36)
 }
 
 export const genId = (prefix?: string, suffix?: string) => {
-    return `${prefix ? `${prefix}-` : ''}${genRandom(6)}-${Date.now().toString(
-        16
-    )}-${genRandom(6)}${suffix ? `-${suffix}` : ''}`.toUpperCase()
+    return `${prefix ? `${prefix}-` : ''}${genRandom(
+        idFormat.phrase_1_length
+    )}-${Date.now().toString(16)}-${genRandom(idFormat.phrase_2_length)}${
+        suffix ? `-${suffix}` : ''
+    }`.toUpperCase()
 }
+
+export const parseId = (id: string) => {}
 
 export class GroupId {
     private readonly _group: string
     private readonly _prefix?: string
     constructor(prefix?: string) {
-        this._group = genRandom(10)
+        this._group = genRandom(idFormat.group_length)
         this._prefix = prefix
     }
     public genId(): string {
