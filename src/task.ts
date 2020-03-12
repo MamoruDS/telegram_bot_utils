4cc52b02bf3d0b75c32b02e2c8c5d8a9764c6461
MamoruDS <mamoruds.io@gmail.com>
2020-03-12 21:21:08 +0900

    added methods setRecordTimeoutLockById, checkRecordsFromPreviousSession and nextCheck in class BotTask
    improved methods checkRecordById and checkRecord with new arguments verifyKey:string and previousRecord:boolean in class BotTask
    
    reworte method checkRecord with 1) key verification 2) timeout check and lock 3) import mode(from cached task records) supports 4) use different import policies
    
    fixed execution order in execTaskByRecordId

--- src/task.ts
+++ src/task.ts
@@ -3,15 +3,23 @@ import * as _ from 'lodash'
 
 import * as cache from './cache'
 import * as utils from './utils'
-import { Task, TaskRecord, TaskRecordMan } from './types'
+import {
+    Task,
+    TaskRecord,
+    TaskRecordMan,
+    importPolicies,
+    ImportPolicy,
+} from './types'
 
 export class BotTask extends events.EventEmitter {
     private _botId: string
     private _tasks: Task[]
+    private _session: utils.GroupId
     constructor(botId: string) {
         super()
         this._botId = botId
         this._tasks = [] as Task[]
+        this._session = new utils.GroupId('TS')
     }
     private getRecords = (): TaskRecord[] => {
         return cache.getTaskRecords(this._botId)
@@ -25,9 +33,12 @@ export class BotTask extends events.EventEmitter {
     }
     public addTask = (task: Task): void => {
         const _task = Object.assign({}, task)
+        _task.interval *= 1000
+        _task.timeout *= 1000
         if (_.filter(this._tasks, { name: _task.name }).length === 0) {
             this._tasks.push(_task)
         }
+        this.checkRecordsFromPreviousSession()
         // TODO: throw error
     }
     private getTask = (taskName: string): Task => {
@@ -44,21 +55,18 @@ export class BotTask extends events.EventEmitter {
             // TODO: throw error
         }
         const startTs = Date.now()
-        const nextTs = execImmediately
-            ? startTs
-            : task.interval * 1000 + startTs
+        const nextTs = execImmediately ? startTs : task.interval + startTs
         const record = {
-            id: utils.genId('TC'),
+            id: this._session.genId(),
             task_name: task.name,
             chat_id: chatId,
             user_id: userId,
-            start_timestamp: startTs,
-            next_timestamp: nextTs,
-            // remains: task.repeat,
-            executed_counts: 0,
+            start: startTs,
+            next: nextTs,
+            executed: 0,
         } as TaskRecord
         this.setRecord(record)
-        this.checkRecordById(record.id)
+        this.nextCheck(record.id)
         return record.id
     }
     public delRecordById = (recordId: string): void => {
@@ -69,47 +77,121 @@ export class BotTask extends events.EventEmitter {
         const _rec = Object.assign({}, record)
         this.setRecord(Object.assign({ expired: true }, _rec))
     }
-    public checkRecordById = (recordId: string): void => {
-        const _rec = this.getRecord(recordId)
-        this.checkRecord(_rec)
+    public checkRecordsFromPreviousSession = () => {
+        const recs = this.getRecords()
+        for (const rec of recs) {
+            if (this._session.isMember(rec.id)) continue
+            this.delRecordById(rec.id)
+            rec.id = this._session.genId()
+            this.setRecord(rec)
+            this.checkRecordById(rec.id, undefined, true)
+        }
     }
-    public checkRecord = (record: TaskRecord | undefined): void => {
+    public setRecordTimeoutLockById = (recordId: string, lock: boolean) => {
+        const _rec = this.getRecord(recordId)
+        if (lock) {
+            _rec.locked = true
+        } else {
+            delete _rec.locked
+        }
+        this.setRecord(_rec)
+    }
+    public checkRecordById = (
+        recordId: string,
+        verifyKey: string,
+        previousRecord?: boolean
+    ): void => {
+        const _rec = this.getRecord(recordId)
+        this.checkRecord(_rec, verifyKey, previousRecord)
+    }
+    public checkRecord = (
+        record: TaskRecord | undefined,
+        verifyKey: string,
+        previousRecord?: boolean
+    ): void => {
         if (!record) return
-        const dTs = Date.now() - record.next_timestamp
+        if (verifyKey !== record.vk && !previousRecord) return
+        const dTs = Date.now() - record.next
         const task = this.getTask(record.task_name)
-        if (record.executed_counts >= task.execution_counts) {
+        if (!task) {
+            return
+        }
+        if (record.executed >= task.execution_counts) {
             this.delRecord(record)
             return
         }
-        if (dTs > task.timeout) {
-            this.emit('timeout', record, task)
-        } else {
-            if (dTs > 0) {
-                this.execTaskByRecordId(record.id)
-            } else {
-                setTimeout(() => {
-                    this.checkRecord(this.getRecord(record.id))
-                }, Math.abs(dTs))
-            }
+        if (dTs > task.timeout && !record.locked) {
+            this.setRecordTimeoutLockById(record.id, true)
+            this.emit('timeout', record, task, previousRecord)
+            return
         }
+        if (dTs > 0 && previousRecord) {
+            const policy =
+                importPolicies.indexOf(task.import_policy) !== -1
+                    ? task.import_policy
+                    : importPolicies[0]
+            switch (policy.split('-')[1]) {
+                case 'ignore':
+                    const curr = Date.now()
+                    while (record.next < curr - task.interval) {
+                        record.next += task.interval
+                    }
+                    break
+                case 'restart':
+                    record.next = Date.now()
+            }
+            this.setRecord(record)
+            if (policy.split('-')[0] === 'curr') {
+                this.execTaskByRecordId(record.id)
+            } else if (policy.split('-')[0] === 'next') {
+                record.next += task.interval
+                this.setRecord(record)
+                this.nextCheck(record.id)
+            }
+        } else if (dTs > 0) {
+            this.execTaskByRecordId(record.id)
+        } else {
+            this.setRecordTimeoutLockById(record.id, false)
+            this.nextCheck(record.id, Math.abs(dTs))
+        }
+    }
+    public nextCheck = (recordId: string, timeout: number = 0) => {
+        const _rec = this.getRecord(recordId)
+        if (_rec) {
+            const key = utils.genRandom(4)
+            _rec.vk = key
+            this.setRecord(_rec)
+            setTimeout(() => {
+                this.checkRecordById(_rec.id, key)
+            }, timeout)
+        }
+    }
+    public execTaskByRecordId = async (recordId: string): Promise<void> => {
+        const record = this.getRecord(recordId)
+        if (!record) return
+        const task = this.getTask(record.task_name)
+        record.next += task.interval
+        record.executed += 1
+        this.setRecord(record)
+        this.emit('execute', record, task)
+        await utils.wait(5)
+        this.nextCheck(record.id)
     }
     public taskRecordMan = (recordId: string): TaskRecordMan => {
         const that = this
         return {
             kill() {
+                that.delRecordById(recordId)
+            },
+            resetTimer(manualTimer?: number) {
                 const _rec = that.getRecord(recordId)
-                that.delRecord(_rec)
+                const _task = that.getTask(_rec.task_name)
+                const _interval =
+                    typeof manualTimer === 'number'
+                        ? manualTimer
+                        : _task.interval
+                _rec.next = Date.now() + _interval
             },
         }
-    }
-    public execTaskByRecordId = (recordId: string): void => {
-        const record = this.getRecord(recordId)
-        if (!record) return
-        const task = this.getTask(record.task_name)
-        record.next_timestamp += task.interval * 1000
-        record.executed_counts += 1
-        this.setRecord(record)
-        this.checkRecordById(record.id)
-        this.emit('execute', record, task)
     }
 }
