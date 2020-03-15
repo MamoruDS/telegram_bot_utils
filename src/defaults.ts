cf933630cffbfa5c3b0c99fdf0fcbd0126e5dd1f
MamoruDS <mamoruds.io@gmail.com>
2020-03-16 08:53:09 +0900

    improved method getOptions with type check
    added methods getOptionsWithAppDefault and options_application
    
    improved methods options_input_listener, options_command and options_task now use default value of 'link_chat/user_free' defined by application

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,28 +1,80 @@
 import * as _ from 'lodash'
 
-import { inputListenerOptions, inputListenerOptionsInput } from './types'
-import { CommandOptions, CommandOptionsInput } from './types'
-import { TaskOptions, TaskOptionsInput } from './types'
+import {
+    Application,
+    ApplicationOptions,
+    ApplicationOptionsInput,
+    getApplication,
+    CommandOptions,
+    CommandOptionsInput,
+    inputListenerOptions,
+    inputListenerOptionsInput,
+    TaskOptions,
+    TaskOptionsInput,
+} from './types'
 import { ActionOptions, ActionOptionsInput } from './types'
 import * as types from './types'
 import * as telegram from './telegram'
 
-export const getOptions = (
+const getOptions = (
     defaultOptions: object,
     userDefinedOptions?: object
 ): object => {
     let _options = Object.assign({}, defaultOptions)
-    if (userDefinedOptions === undefined) {
-        return _options
-    } else {
-        for (const option in _options) {
-            _options[option] =
-                userDefinedOptions[option] !== undefined
-                    ? userDefinedOptions[option]
-                    : _options[option]
-        }
-        return _options
+    if (typeof userDefinedOptions === 'undefined') {
+        userDefinedOptions = {}
+    } else if (
+        typeof userDefinedOptions !== 'object' ||
+        userDefinedOptions === null ||
+        Array.isArray(userDefinedOptions)
+    ) {
+        throw new TypeError('Input options should be an object or undefined')
     }
+    for (const option in _options) {
+        _options[option] =
+            userDefinedOptions[option] !== undefined
+                ? userDefinedOptions[option]
+                : _options[option]
+    }
+    return _options
+}
+
+export const getOptionsWithAppDefault = (
+    inputOptions:
+        | CommandOptionsInput
+        | inputListenerOptionsInput
+        | TaskOptionsInput
+        | undefined,
+    defaultOptions: CommandOptions | inputListenerOptions | TaskOptions,
+    getApplication: getApplication
+): CommandOptions | inputListenerOptions | TaskOptions => {
+    let _options = Object.assign({}, defaultOptions)
+    if (typeof inputOptions === 'undefined') {
+        inputOptions = {}
+    } else if (
+        typeof inputOptions !== 'object' ||
+        inputOptions === null ||
+        Array.isArray(inputOptions)
+    ) {
+        throw new TypeError('Input options should be an object or undefined')
+    }
+    const app = getApplication(
+        inputOptions.application_name || defaultOptions.application_name,
+        true,
+        false
+    )
+    _options = {
+        ..._options,
+        link_chat_free: app.link_chat_free,
+        link_user_free: app.link_user_free,
+    }
+    for (const option in _options) {
+        _options[option] =
+            inputOptions[option] !== undefined
+                ? inputOptions[option]
+                : _options[option]
+    }
+    return _options
 }
 
 export const chat_permissions_unrestricted: telegram.ChatPermissions = {
@@ -47,6 +99,24 @@ export const chat_permissions_fully_restricted: telegram.ChatPermissions = {
     can_pin_messages: false,
 }
 
+const defaultApplicationOptions = {
+    priority: -Infinity,
+    final_app: false,
+    is_group_need_bind: true,
+    link_chat_free: false,
+    link_user_free: false,
+} as ApplicationOptions
+
+export const options_application = (
+    options: ApplicationOptionsInput | undefined
+): ApplicationOptions => {
+    const _options = getOptions(
+        options,
+        defaultApplicationOptions
+    ) as ApplicationOptions
+    return _options
+}
+
 const defaultInputListenerOptions = {
     application_name: types.appGlobal,
     link_chat_free: false,
@@ -59,11 +129,13 @@ const defaultInputListenerOptions = {
 } as inputListenerOptions
 
 export const options_input_listener = (
-    options?: inputListenerOptionsInput
+    options: inputListenerOptionsInput | undefined,
+    getApplication: getApplication
 ): inputListenerOptions => {
-    const _options = getOptions(
+    const _options = getOptionsWithAppDefault(
+        options,
         defaultInputListenerOptions,
-        options
+        getApplication
     ) as inputListenerOptions
     return _options
 }
@@ -80,11 +152,13 @@ const defaultCommandOptions = {
 } as CommandOptions
 
 export const options_command = (
-    options?: CommandOptionsInput
+    options: CommandOptionsInput | undefined,
+    getApplication: getApplication
 ): CommandOptions => {
-    const _options = getOptions(
+    const _options = getOptionsWithAppDefault(
+        options,
         defaultCommandOptions,
-        options
+        getApplication
     ) as CommandOptions
     return _options
 }
@@ -99,8 +173,15 @@ const defaultTaskOptions = {
     timeout_action: async () => {},
 } as TaskOptions
 
-export const options_task = (options?: TaskOptionsInput): TaskOptions => {
-    const _options = getOptions(defaultTaskOptions, options) as TaskOptions
+export const options_task = (
+    options: TaskOptionsInput | undefined,
+    getApplication: getApplication
+): TaskOptions => {
+    const _options = getOptionsWithAppDefault(
+        options,
+        defaultTaskOptions,
+        getApplication
+    ) as TaskOptions
     return _options
 }
 
