dacc93bbb4e298f9d305692c43f96a3fabba6be4
MamoruDS <mamoruds.io@gmail.com>
2020-04-11 03:38:43 +0900

    removed method cmdMatch
    rewrote class Command & CommandMgr with typed constructor support CTR
    
    improved use ApplicationInfo instead old properties in new command options
    
    added method argumentErrorHandle in class Command

--- src/command.ts
+++ src/command.ts
@@ -1,31 +1,11 @@
-import { getApplication, applicationDataMan } from './types'
-import { CTR } from './ctr'
 import { Message } from './telegram'
-import * as types from './types'
 import { botMgr } from './main'
-import { ApplicationDataMan } from './application'
-
-export const cmdMatch = (input: string): string[] | null => {
-    const regex = new RegExp(
-        `(^\\/([\\w|_|\\-|\\.]{1,})(?=$|\\s))|([^\\s]{1,})`,
-        'g'
-    )
-    let result = regex.exec(input)
-    /**
-     * result [0] -> input
-     * result [1] -> group_1 \/cmd\s
-     * result [2] -> group_2 cmd
-     * result [3] -> group_3 args
-     */
-    let args = []
-    args.push(result ? result[2] : undefined)
-    if (args[0] !== undefined) {
-        while ((result = regex.exec(input)) !== null) args.push(result[3])
-        return args
-    } else {
-        return null
-    }
-}
+import {
+    ApplicationDataMan,
+    ApplicationInfo,
+    AppBaseUtilCTR,
+    AppBaseUtilItem,
+} from './application'
 
 export const parseCommand = (
     input: string
@@ -60,6 +40,7 @@ export const argumentCheck = async (
     for (const i of Array(checker.length).keys()) {
         const indexFixed = i + 1
         let _arg: any = _args[indexFixed]
+        const _type = checker[i].type
         if (_arg === undefined) {
             const _defaultValue = checker[i].default_value
             if (_defaultValue === undefined) {
@@ -68,13 +49,12 @@ export const argumentCheck = async (
                 // }
                 // TODO: error adv
                 throw new TypeError(
-                    `Required argument (args[${indexFixed}]) do not exist.`
+                    `Except argument as ${_type} at position[${indexFixed}] does not exist.`
                 )
             } else {
                 _arg = _defaultValue
             }
         } else {
-            const _type = checker[i].type
             if (_type === 'string') {
                 // pass
             } else if (_type === 'integer') {
@@ -97,7 +77,7 @@ export const argumentCheck = async (
             }
             if (typeof _arg === 'undefined')
                 throw new TypeError(
-                    `Cannot parse given argument (args[${indexFixed}]="${args[indexFixed]}") to the type required.`
+                    `Cannot parse given argument "${args[indexFixed]}" at position[${indexFixed}] to the type required (${_type}).`
                 )
             const _range = checker[i].range
             if (_range !== undefined) {
@@ -107,7 +87,7 @@ export const argumentCheck = async (
                 } else if (_range.length !== 0) {
                     if (_range.indexOf(_arg) === -1)
                         throw new RangeError(
-                            `Given argument (args[${indexFixed}]="${args[indexFixed]}") out of range.`
+                            `Given argument "${args[indexFixed]}" at position[${indexFixed}] was out of range.`
                         )
                 }
             }
@@ -117,41 +97,48 @@ export const argumentCheck = async (
     return _args
 }
 
-export class CommandMgr extends CTR<Command> {
-    idField = 'cmd'
-    itemType = 'Command'
-    private botName: string
-
+export class CommandMgr extends AppBaseUtilCTR<Command, CommandConstructor> {
     constructor(botName: string) {
-        super(Command)
-        this.botName = botName
+        super(Command, 'Command', 'cmd', botName)
     }
 
     add(
         commandString: string,
         execFunc: CommandExecuteFunction,
-        options: CommandOptions = {}
+        options: CommandOptions = {},
+        applicationInfo: ApplicationInfo
     ) {
-        return super.add(commandString, execFunc, options, this.botName)
+        return super.add(
+            commandString,
+            execFunc,
+            options,
+            applicationInfo,
+            this._botName
+        )
     }
-    check = (msg: Message): void => {
-        const cmdInfo = parseCommand(msg.text || msg.caption)
+    async check(message: Message): Promise<void> {
+        const cmdInfo = parseCommand(message.text || message.caption)
         if (!cmdInfo.matched) return
-        if (cmdInfo.botMentioned && cmdInfo.botMentioned !== this.botName)
+        if (cmdInfo.botMentioned && cmdInfo.botMentioned !== this._botName)
             return
         const cmd = this.get(cmdInfo.args[0])
         if (typeof cmd === 'undefined') return
         if (
             !botMgr
-                .get(this.botName)
-                .application.get(cmd.dataSpace.application_name)
-                .isValidForChat(msg.chat)
+                .get(this._botName)
+                .application.get(cmd.appInfo.application_name)
+                .isValidForChat(message.chat)
         ) {
             return
         }
-        const dataMan = cmd.dataMan(msg)
-        if (!cmd.messageFilter(msg)) return
-        cmd.exec(cmdInfo.args, msg, dataMan)
+        const dataMan = cmd.dataMan(message)
+        if (!cmd.messageFilter(message)) return
+        try {
+            const _args = await argumentCheck(cmdInfo.args, cmd.check)
+            cmd.exec(_args, message, dataMan)
+        } catch (e) {
+            cmd.argumentErrorHandle(message, e)
+        }
         return
     }
 }
@@ -168,78 +155,78 @@ type CommandFilter = 'public' | 'registered' | 'owner' | 'function'
 type CommandExecuteFunction = (
     args: string[],
     message: Message,
-    data: applicationDataMan
+    data: ApplicationDataMan
 ) => void
 
 export interface CommandOptions {
-    application_name?: string
     argument_check?: ArgumentCheck[]
-    argument_error_function?: (msg: Message, err: Error) => any
-    link_chat_free?: boolean
-    link_user_free?: boolean
+    argument_error_function?: (message: Message, err: Error) => any
     filter?: CommandFilter
-    filter_function?: (msg: Message) => Promise<boolean>
+    filter_function?: (message: Message) => Promise<boolean>
     description?: string
 }
 
 const defaultCommandOptions: Required<CommandOptions> = {
-    application_name: types.appGlobal,
     argument_check: [],
-    argument_error_function: () => {},
-    link_chat_free: false,
-    link_user_free: false,
+    argument_error_function: (msg, err) => {
+        throw err
+    },
     filter: 'owner',
     filter_function: async () => true,
     description: 'undefined',
 }
 
-class Command {
-    private readonly botName: string
+interface CommandConstructor {
+    new (
+        commandString: string,
+        execFunc: CommandExecuteFunction,
+        options: CommandOptions,
+        appInfo: ApplicationInfo,
+        botName: string
+    ): Command
+}
+
+class Command extends AppBaseUtilItem {
     private readonly _command: string
     private readonly _execFunction: CommandExecuteFunction
-    private readonly _applicationName: string
     private readonly _argumentCheck: ArgumentCheck[]
-    private readonly _argumentErrorFunction: (msg: Message, err: Error) => any
-    private readonly _linkChatFree: boolean
-    private readonly _linkUserFree: boolean
+    private readonly _argumentErrorFunction: (
+        message: Message,
+        err: Error
+    ) => any
     private _filter: CommandFilter
-    private _filterFunction: (msg: Message) => Promise<boolean>
+    private _filterFunction: (message: Message) => Promise<boolean>
     private _description?: string
 
+    constructor(...P: ConstructorParameters<CommandConstructor>)
     constructor(
         commandString: string,
         execFunc: CommandExecuteFunction,
-        options: CommandOptions = {},
+        options: CommandOptions,
+        appInfo: ApplicationInfo,
         botName: string
     ) {
-        this.botName = botName
+        super(appInfo, botName)
         const _options = botMgr
-            .get(this.botName)
+            .get(this._botName)
             .getDefaultOptions<CommandOptions>(defaultCommandOptions, options)
         this._command = commandString
         this._execFunction = execFunc
-        this._applicationName = _options.application_name
         this._argumentCheck = _options.argument_check
         this._argumentErrorFunction = _options.argument_error_function
-        this._linkChatFree = _options.link_chat_free || false
-        this._linkUserFree = _options.link_user_free || false
         this._filter = _options.filter
         this._filterFunction = _options.filter_function
         this._description = _options.description
     }
 
     private get CTR(): CommandMgr {
-        return botMgr.get(this.botName).command
+        return botMgr.get(this._botName).command
     }
     get cmd(): string {
         return this._command
     }
-    get dataSpace(): types.DataSpace {
-        return {
-            application_name: this._applicationName,
-            link_chat_free: this._linkChatFree,
-            link_user_free: this._linkUserFree,
-        }
+    get check(): ArgumentCheck[] {
+        return this._argumentCheck
     }
     get filter(): string {
         return this._filter
@@ -248,37 +235,32 @@ class Command {
         return this._description
     }
 
-    dataMan = (msg: Message): ApplicationDataMan => {
-        return botMgr
-            .get(this.botName)
-            .application.get(this._applicationName)
-            .dataMan({
-                chat_id: this._linkChatFree ? undefined : msg.chat.id,
-                user_id: this._linkUserFree ? undefined : msg.from.id,
-            })
-    }
-    messageFilter = async (msg: Message): Promise<boolean> => {
+    async messageFilter(message: Message): Promise<boolean> {
         if (this._filter === 'public') {
             return true
         }
         if (this._filter === 'registered') {
-            const _data = this.dataMan(msg).data
+            const _data = this.dataMan(message).data
             return Object.keys(_data).length !== 0
         }
         if (this._filter === 'owner') {
             return (
-                msg.from.id === botMgr.get(this.botName).owner.id ||
-                msg.from.username === botMgr.get(this.botName).owner.username
+                message.from.id === botMgr.get(this._botName).owner.id ||
+                message.from.username ===
+                    botMgr.get(this._botName).owner.username
             )
         }
         if (this._filter === 'function') {
-            return await this._filterFunction(msg)
+            return await this._filterFunction(message)
         }
         return false
     }
-    exec = (
+    exec(
         ...args: Parameters<CommandExecuteFunction>
-    ): ReturnType<CommandExecuteFunction> => {
+    ): ReturnType<CommandExecuteFunction> {
         return this._execFunction(...args)
+    }
+    argumentErrorHandle(message: Message, err: Error) {
+        this._argumentErrorFunction(message, err)
     }
 }
