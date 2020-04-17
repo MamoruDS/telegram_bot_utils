515117a71bdd50c4e32ee8ff695cb904fbdeca4a
MamoruDS <mamoruds.io@gmail.com>
2020-04-17 22:01:36 +0900

    added class GroupUtils with group event handle

--- src/group.ts
+++ src/group.ts
@@ -1,15 +1,50 @@
+import { EventEmitter } from 'events'
 import { Message } from './telegram'
 
-export const joinListener = (msg: Message, func: (msg: Message) => any) => {
-    const newMembers = msg.new_chat_members ? msg.new_chat_members : []
-    if (newMembers.length !== 0) {
-        func(msg)
-    }
-}
+export class GroupUtils extends EventEmitter {
+    private _botName: string
+    private _triggeredByBot: boolean
+    private _triggeredBySelf: boolean
 
-export const leftListener = (msg: Message, func: (msg: Message) => any) => {
-    const leftMember = msg.left_chat_member ? msg.left_chat_member : undefined
-    if (leftMember) {
-        func(msg)
+    constructor(
+        botName: string,
+        options: {
+            triggeredByBot?: boolean
+            triggeredBySelf?: boolean
+        } = {}
+    ) {
+        super()
+        this._botName = botName
+        this._triggeredByBot = options.triggeredByBot
+        this._triggeredBySelf = options.triggeredBySelf
+    }
+
+    listener = (msg: Message) => {
+        const manMember = msg.from
+        const leftMember = msg.left_chat_member
+        if (leftMember) {
+            if (leftMember.id === manMember.id) {
+                this._trigger('left', msg, leftMember)
+            } else {
+                this._trigger('removed', msg, leftMember)
+            }
+        }
+        const newMembers = msg.new_chat_members || []
+        if (newMembers.length !== 0) {
+            for (const i in newMembers) {
+                const newMember = newMembers[i]
+                if (newMember.id === manMember.id) {
+                    this._trigger('joined', msg, newMember)
+                } else {
+                    this._trigger('added', msg, newMember)
+                }
+            }
+        }
+    }
+    private _trigger = (event: string, msg: Message, ...args: any[]) => {
+        if (msg.from.is_bot && !this._triggeredByBot) return
+        if (msg.from.username === this._botName && !this._triggeredBySelf)
+            return
+        this.emit(event, msg, ...args)
     }
 }
