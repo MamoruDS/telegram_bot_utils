627762ab48c2babae2d84e528aa41aaa052b0180
MamoruDS <mamoruds.io@gmail.com>
2020-04-24 04:29:06 +0900

    improved module entrance with better external api ('node-telegram-bot-api') import

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,13 @@
 import { BotMgr } from './bot'
-import * as botAPI from 'node-telegram-bot-api'
 
-const _bots = new BotMgr()
-const _defaults = {
+const _bots = new BotMgr() as BotMgr
+
+const _options = {
+    botAPIConstructor: undefined,
     maxInlineButtonWidth: 16,
 } as {
+    botAPIConstructor: new () => any
     maxInlineButtonWidth: number
 }
-const _botAPI: botAPI = undefined
 
-const _hasBotAPIModule = () => {
-    try {
-        const _b = new botAPI('N', { polling: false })
-        return typeof _b.closeWebHook === 'function'
-    } catch (e) {
-        return false
-    }
-}
-
-export {
-    _bots as bots,
-    _defaults as defaults,
-    _botAPI as api,
-    _hasBotAPIModule as hasBotAPIModule,
-}
-
+export { _bots as bots, _options as options }
